eb501f46eee05328705cf5800e6fadd8937c7ed4
derekschwabe <derek.schwabe12@houghton.edu>
2015-10-27 18:45:51 -0400

    Update sponsors.js

--- js/sponsors.js
+++ js/sponsors.js
@@ -2,9 +2,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() { 
   $('#slideshow > div:first')
-    .hide('slide', {direction: 'right'}, 1000)
+    .fadeOut(1000)
     .next()
-    .show('slide', {direction: 'left'}, 1000)
+    .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
 },  3000);
